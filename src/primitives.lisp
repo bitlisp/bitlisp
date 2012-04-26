f5b7e5076f0d4ca36e2e1895db06603e74183f8a
Benjamin Saunders <besaunde@sfu.ca>
2012-04-26 13:50:15 -0700

    Need to actually return values from these.

--- src/primitives.lisp
+++ src/primitives.lisp
@@ -191,7 +191,7 @@
 
 (defprimpoly "vector-elt" (ty size) ty ((vector `("vector" ,ty ,size)) (index '("int" 32))) (builder)
   ;; TODO: Optional bounds checking
-  (llvm:build-extract-element builder vector index ""))
+  (llvm:build-ret builder (llvm:build-extract-element builder vector index "")))
 
 (defprimpoly "bitcast" (a b) b ((x a)) (builder)
-  (llvm:build-bit-cast builder x (llvm b) ""))
+  (llvm:build-ret builder (llvm:build-bit-cast builder x (llvm b) "")))
